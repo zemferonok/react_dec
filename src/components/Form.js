cf3da0c852dd3678e748af99d87da4cdbb7d11ba
zemferonok <zemferonok@gmail.com>
2022-04-14 18:43:44 +0300

    Fin + Add comments

---
+++ src/components/Form.js
@@ -0,0 +1,27 @@
+import {useForm} from "react-hook-form";
+
+const Form = () => {
+    const {register, handleSubmit, reset, setValue} = useForm();
+    // register - has a many base fields
+
+    const mySubmit = (data) => {
+        console.log(data)       // All data from form
+        reset()                 // Clear all fields of Form
+    }
+
+    const someSet = () => {
+        setValue('name', 'Lolita')  // Set in field 'name'
+        setValue('age', 16)         // Set in field 'age'
+    }
+
+    return (
+        <form onSubmit={handleSubmit(mySubmit)}>  {/* Make prevent default and some more */}
+            <label>Name: <input type="text" {...register('name')}/></label>
+            <label>Age: <input type="text" {...register('age', {valueAsNumber: true})}/></label>
+            <button>Submit</button>     {/* As default all Form's input data is string */}
+            <a href="#" onClick={someSet}>setValue</a>
+        </form>
+    );
+};
+
+export {Form};
